5ed65b1f1281186619b9f80a0275b752a69abec5
Mahmud Munna <smbmunna@gmail.com>
2023-02-21 00:13:46 +0600

    From To form in Destination added

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,11 +1,25 @@
 import React, { useContext } from 'react';
 import Header from '../Header/Header';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const Destination = ({children}) => {
+const Destination = ({ children }) => {
     return (
         <div>
             <Header></Header>
-            <h3>This is destination</h3>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Pick From</Form.Label>
+                    <Form.Control name="from" placeholder="From Destination" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Pick To</Form.Label>
+                    <Form.Control name="to" placeholder="To Destination" />
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    Search
+                </Button>
+            </Form>
         </div>
     );
 };
